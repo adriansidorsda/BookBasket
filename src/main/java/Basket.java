75509750317d6a43efee29191e0f885c9bc783bc
Adrian Sidor <adriansidor11@wp.pl>
2020-01-18 19:32:54 +0100

    Implementation Basket and Book.

--- src/main/java/Basket.java
+++ src/main/java/Basket.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Basket {
 
-   static List<Book> books = new ArrayList<>();
+    List<Book> books = new ArrayList<>();
 
     public Book createBook(String bookName, String authorName, int price) {
         return new Book(bookName, authorName, price);
